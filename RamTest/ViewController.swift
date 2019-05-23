1d13cf668cc453f0b803bf48295824740686a01f
D7703_22 <d7703_22@promise.local>
2019-05-23 11:23:10 +0900

    Initial Commit

---
+++ RamTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RamTest
+//
+//  Created by D7703_22 on 2019. 5. 23..
+//  Copyright © 2019년 fb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
