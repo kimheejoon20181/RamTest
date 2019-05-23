d73935af56b02f71c9943b1672887fffb70674c7
D7703_22 <d7703_22@promise.local>
2019-05-23 12:39:43 +0900

    RAM 활용

---
+++ Pods/Target Support Files/Pods-RamTest/Pods-RamTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RamTest : NSObject
+@end
+@implementation PodsDummy_Pods_RamTest
+@end
--- RamTest/ViewController.swift
+++ RamTest/ViewController.swift
@@ -7,9 +7,18 @@
 //
 
 import UIKit
+import RealmSwift
+
+class Person: Object {
+    @objc dynamic var name = " "
+    @objc dynamic var age = 0
+}
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var txtV: UITextField!
+    @IBOutlet weak var txtA: UITextField!
+    @IBOutlet weak var txtView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,43 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btnAdd(_ sender: Any) {
+        let person = Person{}
+        
+        person.name = txtV.text!
+        person.age = Int(txtA.text!)!
+            
+        let realm = try! Realm()
+        try! realm.write {
+            realm.add(person)
+        }
+        
+        txtView.text = ""
+        txtA.text = ""
+    }
+    
+    @IBAction func btnDeleteAll(_ sender: Any) {
+        let realm = try! Realm()
+        try! realm.write {
+            realm.deleteAll()
+        }
+        
+        txtView.text = ""
+    }
+    
+    @IBAction func btnGet(_ sender: Any) {
+        txtView.text = ""
+        
+        let realm = try! Realm()
+        let vals = realm.objects(Person.self)
+        
+        if vals.count == 0{
+            return
+        } else{
+            for i in vals {
+                txtView.text = txtView.text! + "Name: \(i.name) Age: \(i.age)\n"
+            }
+        }
+    }
 }
 
